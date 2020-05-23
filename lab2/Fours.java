58baf470e3a9755474c45869abcb7938f74d56b7
maseDevelop <37942066+maseDevelop@users.noreply.github.com>
2020-05-23 20:21:29 +1200

    Update bugs

--- lab2/Fours.java
+++ lab2/Fours.java
@@ -1,19 +1,19 @@
 	import java.util.*;
 
-	public class Fours {
+public class Fours {
 
-		private static Queue<String> queueFrontier = new LinkedList<>();
-		private static String startState = "4";
+	private static Queue<String> queueFrontier = new LinkedList<>();
+	private static String startState = "4";
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 
+		if (args.length == 1) {
 			try {
 
 				String currState;
-				Double startNum = (double)1;
+				Double startNum = Double.parseDouble(args[0]);
 				Double returnedNum;
 
-				while(startNum <= 100){
 					// Getting the start state ready
 					queueFrontier.add(startState);
 
@@ -24,44 +24,44 @@
 						if (currState != null) {
 							// Check if the equation is viable then print it out as a success
 							returnedNum = EvaluateString.evaluate(currState);
-							//System.out.println("Returned Num: " + returnedNum);
-							//System.out.println(currState);
 
-							if(returnedNum != null && Double.compare(startNum,returnedNum) == 0){
+							if (returnedNum != null && Double.compare(startNum, returnedNum) == 0) {
 								System.out.println("FOUND SOLUTION: " + returnedNum + " for " + currState);
-								startNum++;
-							}
-							else if(returnedNum != null){		
+								System.exit(0);
+							} else if (returnedNum != null) {
 								// Expand the state space if a valid expression
 								expand(currState);
 							}
 						}
 					}
-				}
+				
 			} catch (Exception e) {
-				System.out.println("ahh you Run out of memory...");
+				System.out.println("opps something went wrong... Make sure you are entering a valid number\n");
 				System.out.println("Error" + e);
 			}
-		
+		} else {
+			System.out.println("Usage: java Fours <number you want to find solution for>");
 		}
 
-		private static void expand(String CurrentState) {
-			String[] returnValues = new String[8];
+	}
 
-			// Concating operators
-			returnValues[0] = CurrentState + " + 4";
-			returnValues[1] = CurrentState + " - 4";
-			returnValues[2] = CurrentState + " * 4";
-			returnValues[3] = CurrentState + " / 4";
-			returnValues[4] = CurrentState + " ** 4";
-			returnValues[5] = CurrentState + "4";
-			returnValues[6] = CurrentState + ".4";
-			returnValues[7] = "(" + CurrentState + ")";
+	private static void expand(String CurrentState) {
+		String[] returnValues = new String[8];
 
-			// Putting new states on the frontier
-			for (int i = 0; i < returnValues.length; i++) {
-				queueFrontier.add(returnValues[i]);
-				returnValues[i] = null;
-			}
+		// Concating operators
+		returnValues[0] = CurrentState + " + 4";
+		returnValues[1] = CurrentState + " - 4";
+		returnValues[2] = CurrentState + " * 4";
+		returnValues[3] = CurrentState + " / 4";
+		returnValues[4] = CurrentState + " ** 4";
+		returnValues[5] = CurrentState + "4";
+		returnValues[6] = CurrentState + ".4";
+		returnValues[7] = "(" + CurrentState + ")";
+
+		// Putting new states on the frontier
+		for (int i = 0; i < returnValues.length; i++) {
+			queueFrontier.add(returnValues[i]);
+			returnValues[i] = null;
 		}
 	}
+}
